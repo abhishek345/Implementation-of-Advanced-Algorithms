79508b343d7a16ea6882364b5b3b85480b48890a
umangkshah <umang.k.shah@gmail.com>
2017-09-01 15:41:20 -0500

    Single init of params

--- SP1/BreadthFirstSearch.java
+++ SP1/BreadthFirstSearch.java
@@ -1,7 +1,7 @@
 /**
  *  Performs BFS on a given graph and returns path to farthest node.
  *  @author Umang Shah, Vibha Belavadi, Abhishek Jagwani, Shreya Rao
- *  Ver 1.0: 2017/08/28.  
+ *  Ver 1.1: 2017/09/01.  
  *
  */
  
@@ -10,12 +10,6 @@ package cs6301.g21;
 import java.util.LinkedList;
 
 public class BreadthFirstSearch {
-
-    class DiameterVertex{
-        // Currently 2 parallel arrays - visited and parent are used
-        // this can be replaced by extending the Vertex class similar to CCVertex used by prof in CC graph example
-        // the new variables for visited and parent can be then replaced in the BFS program
-    }
 
     static LinkedList<Graph.Vertex> bfs(Graph g, Graph.Vertex v){
         Graph.Vertex farthestNode = v;
@@ -26,10 +20,9 @@ public class BreadthFirstSearch {
         //array to store parent in BFS path
         Graph.Vertex[] parent = new Graph.Vertex[g.size()];
         //initalize to default values
-        for(int i=0;i < parent.length; i++)
-            parent[i] = null;
         for(int i=0;i < visited.length;i++){
             visited[i] = false;
+            parent[i] = null;
         }
         
         //Add starting vertex to the queue
