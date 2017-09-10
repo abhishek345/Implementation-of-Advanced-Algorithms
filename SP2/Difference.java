b34dff296e92ea4ee467707d882db79a6eaef1e7
Abhishek Jagwani <abhi.jagwani@gmail.com>
2017-09-09 19:06:08 -0500

    Update Difference.java

--- SP2/Difference.java
+++ SP2/Difference.java
@@ -1,10 +1,24 @@
 package cs6301.g21;
 
+/**
+ * Implementation of Difference operator on List
+ * @author Abhishek Jagwani, Umang Shah, Vibha  Belavadi, Shreya Vishwanath Rao
+ * @version 1.0 : 09/01/2016
+ *
+ */
+
 import java.util.List;
 import java.util.Iterator;
 
 public class Difference {
 
+	/**
+	 * Function used to compute Difference of Lists type T
+	 * @param l1 : List l1 of type T passed from Main function
+	 * @param l2 : List l1 of type T passed from Main function
+	 * @param outList : Output list to store the intersection of 2 input lists
+	 */	
+	
 	public static <T extends Comparable<? super T>> void difference(List<T> l1, List<T> l2, List<T> outList){
 		
 		Iterator<T> it1 = l1.iterator();
@@ -17,6 +31,7 @@ public class Difference {
 		while(temp1!=null && temp2!=null){
 			int comp = temp1.compareTo(temp2);
 			
+			//if element of l1 is smaller than element of l2
 			if(comp<0){
 				if(prev != temp1)
 					outList.add(temp1);
@@ -26,12 +41,14 @@ public class Difference {
 				temp1=it1.next();
 			}
 			
+			//if element of l2 is smaller than element of l1
 			else if(comp>0){
 				if(!it2.hasNext())
 					break;
 				temp2=it2.next();
 			}
 			
+			//if both the elements of l1 and l2 are equal
 			else{
 				if(!it1.hasNext() || !it2.hasNext())
 					break;
@@ -40,6 +57,8 @@ public class Difference {
 			}	
 		}
 		temp1 = it1.next();
+		
+		//if l1 is not empty yet
 		while(temp1 != null){			
 			if(prev != temp1)
 				outList.add(temp1);
