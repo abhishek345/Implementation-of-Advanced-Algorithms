c10d7f5b38bfdd8002ccd817b8e5e7b72e80aebc
Abhishek Jagwani <abhi.jagwani@gmail.com>
2017-09-09 19:06:28 -0500

    Update LinkedListQuestion.java

--- SP2/LinkedListQuestion.java
+++ SP2/LinkedListQuestion.java
@@ -5,13 +5,25 @@ import java.util.ArrayList;
 import java.util.Scanner;
 import java.io.File;
 
+/**
+ * Implementation of Intersection, Union, and Set difference operators on List
+ * @author Abhishek Jagwani, Umang Shah, Vibha  Belavadi, Shreya Vishwanath Rao
+ * @version 1.0 : 09/01/2016
+ *
+ */
 
 public class LinkedListQuestion {
-
+	
+	/**
+	 * Function used to read input file provided from command line and create two different lists
+	 * @param in : Input file of elements scanned to create lists
+	 * @param l1 : ArrayList l1 to be created from input file
+	 * @param l2 : ArrayList l2 to be created from input file
+	 */
 	static void createArrayList(Scanner in, ArrayList<Integer> l1, ArrayList<Integer> l2){
 		
 		int n = in.nextInt();
-				
+			
 		for(int i = 0; i < n; i++){
 			int temp = in.nextInt();
 			l1.add(temp);
@@ -26,24 +38,24 @@ public class LinkedListQuestion {
 		
 	}
 	
+	/**
+	 * Main function used to scan input file of elements and call function to create lists and perform operations on lists
+	 * @param args : command line input
+	 * @throws FileNotFoundException : Exception if no file is provided as input or no file found
+	 */
 	
 	public static void main(String[] args) throws FileNotFoundException{
-		
+	
 		ArrayList<Integer> l1 = new ArrayList<Integer>();
 		ArrayList<Integer> l2 = new ArrayList<Integer>();
 		
 		Scanner sf = new Scanner(new File("linkedList.txt"));
 		createArrayList(sf,l1,l2);
-	//	System.out.println(l1);
-	//	System.out.println(l2);
 		ArrayList<Integer> outList1 = new ArrayList<Integer>();
 		Intersection.intersect(l1,l2,outList1);
-	//	System.out.println(outList1);
 		ArrayList<Integer> outList2 = new ArrayList<Integer>();
 		Union.union(l1, l2, outList2);
-	//	System.out.println(outList2);
 		ArrayList<Integer> outList3 = new ArrayList<Integer>();
 		Difference.difference(l1,l2,outList3);
-	//	System.out.println(outList3);
 	}
 }
