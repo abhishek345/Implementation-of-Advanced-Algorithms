38af1056a90644ac2166b48aea6d06ce8c983f95
Shreya Vishwanath Rao <ShreyaRao09@users.noreply.github.com>
2017-09-15 15:12:39 -0500

    Add files via upload
    used by Topological.java and SCC.java

--- SP3/src/cs6301/g21/GraphExtended.java
+++ SP3/src/cs6301/g21/GraphExtended.java
@@ -1,26 +1,90 @@
 package cs6301.g21;
 
-public class GraphExtended {
-
-    class ExtendedVertex{
-        Graph.Vertex element;
-        boolean seen;
-        int cno;
-        ExtendedVertex(Graph.Vertex u) {
-            element = u;
-            seen = false;
-            cno = -1;
-        }
-    }
-
-    ExtendedVertex[] xVertex;
-    Graph g;
-
-    public GraphExtended(Graph g) {
-        this.g = g;
-        xVertex= new ExtendedVertex[g.size()];
-        for(Graph.Vertex u: g) { xVertex[u.name] = new ExtendedVertex(u); }
-    }
-
+public class GraphExtended{
+	
+	Graph g;
+	boolean seen[];
+	int dis[];
+	int fin[];
+	int vCno[];
+	int top[];
+	int parent[];
+	int inDegree[];
+	
+	
+	public GraphExtended(Graph g) {
+		this.g= g;
+		seen= new boolean[g.size()];
+		dis = new int[g.size()];
+		fin = new int[g.size()];
+		vCno = new int[g.size()];
+		top = new int[g.size()];
+		parent = new int[g.size()];
+		inDegree= new int[g.size()];
+		
+	}
+	
+	public void setSeen(int index,boolean value){
+		seen[index]=value;
+	}
+	
+	public boolean getSeen(int index){
+		return seen[index];
+	}
+	
+	
+	public void setDis(int index,int value){
+		dis[index]=value;
+	}
+	
+	public int getDis(int index){
+		return dis[index];
+	}
+	
+	public void setFin(int index,int value){
+		fin[index]=value;
+	}
+	
+	public int getFin(int index){
+		return fin[index];
+	}
+	
+	public void setVCno(int index,int value){
+		vCno[index]=value;
+	}
+	
+	public int getVCno(int index){
+		return vCno[index];
+	}
+	
+	public void setTop(int index,int value){
+		top[index]=value;
+	}
+	
+	public int getTop(int index){
+		return top[index];
+	}
+	
+	public void setParent(int index,int value){
+		parent[index]=value;
+	}
+	
+	public int getParent(int index){
+		return parent[index];
+	}
+	
+	public void setInDegree(int index,int value){
+		inDegree[index]=value;
+	}
+	
+	public int getInDegree(int index){
+		return inDegree[index];
+	}
+	
+	public int size(){
+		return g.size();
+	}
+	
+	
 
 }
