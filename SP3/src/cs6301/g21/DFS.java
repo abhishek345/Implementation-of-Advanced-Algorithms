66f4b3964fb2c3a09f6806962804fa7b98c852e9
Shreya Vishwanath Rao <ShreyaRao09@users.noreply.github.com>
2017-09-16 12:32:03 -0500

    Update DFS.java

--- SP3/src/cs6301/g21/DFS.java
+++ SP3/src/cs6301/g21/DFS.java
@@ -1,6 +1,77 @@
 package cs6301.g21;
 
+import java.util.Iterator;
+import java.util.LinkedList;
+
+/**
+ *
+ * Depth First Search Implementation
+ * 
+ * @author Shreya Vishwanath Rao, Abhishek Jagwani, Vibha Belavadi, Umang Shah
+ * @version 1.0: 2017/09/13
+ *
+ */
+
 public class DFS {
+	
+	static int time;
+	static int topNum;
+	static LinkedList<Graph.Vertex> decFinList;
 
+	/**
+	 * Depth First Search implementation
+	 * 
+	 * @param it : order in which DFS must be implemented 
+	 * @param ge : Graph extended object containing the graph
+	 * @return   : LinkedList<Graph.Vertex> : decreasing finish order time of the vertices in the graph
+	 */
+	public static LinkedList<Graph.Vertex> DFS(Iterator it,GraphExtended ge){
+		topNum=ge.size();
+		time=0;
+		ge.cno=0;
+		decFinList= new LinkedList<Graph.Vertex>();
 
+		for(int i=0;i<ge.size();i++)
+			ge.setSeen(i,false);
+		while(it.hasNext()){
+			Graph.Vertex u= (Graph.Vertex)it.next();
+			if(!ge.getSeen(u.getName())){
+				ge.cno++;
+				DFSVisit(u,ge);
+			}
+		}
+		return decFinList;
+	}
+	
+	/**
+	 * Traverses through all the adjacent unvisited vertices of the vertex
+	 * sent to the function.
+	 * Stores the topological number, discovery and finish time of the vertex. 
+	 * It add the vertex to the finish order list once all its neighbors
+	 * have been visited.
+	 * 
+	 * @param u : the vertex whose neighbors have to be traversed through
+	 * @param ge :GraphExtended object containing the graph and other details of the vertex
+	 */
+	public static void DFSVisit(Graph.Vertex u, GraphExtended ge){
+		int uName= u.getName();
+		ge.setSeen(uName,true);
+		ge.setDis(uName,++time);
+		ge.setVCno(uName,ge.cno);
+		
+		Iterator adjEdges = u.adj.iterator();
+		
+		while(adjEdges.hasNext()){
+			Graph.Edge e= (Graph.Edge)adjEdges.next();
+			Graph.Vertex v= e.otherEnd(u);
+			if(!ge.getSeen(v.getName())){
+				ge.setParent(v.getName(),uName);
+				DFSVisit(v,ge);
+			}
+		}
+		ge.setFin(uName,++time);
+		ge.setTop(uName,topNum--);
+		decFinList.addFirst(u);
+		
+	}
 }
