c3369bc0dc75057a3a9dc4506cfe263e605df642
umangkshah <umang.k.shah@gmail.com>
2017-09-18 22:29:44 -0500

    class for operations and l4 driver

--- LP-1/src/cs6301/g21/LP1L4.java
+++ LP-1/src/cs6301/g21/LP1L4.java
@@ -1,8 +1,61 @@
 package cs6301.g21;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Scanner;
+
 public class LP1L4 {
 
-    public static void main(String args[]){
+    public static void main(String args[])throws FileNotFoundException{
+        LinkedList<Operation> program = new LinkedList<>();
+        HashMap<String, Operation> access = new HashMap<>();
 
+        if(args.length > 0){
+            Scanner in = new Scanner(new File(args[0]));
+            while (in.hasNextLine()){
+                String line = in.nextLine();
+                if(line.equals(";"))
+                    break;
+                Operation op = new Operation();
+                int type=-1;
+                if(line.contains("+...")) {
+                    op.setType(2);
+                    type = 2;
+                }else if(line.contains("?")){
+                    op.setType(3);
+                    type = 3;
+                }
+                String [] tokens = line.split(" ");
+                for(int i=0;i < tokens.length;i++){
+                    if(tokens[i].matches("\\d+")){
+                        if(i==0) {
+                            String lno = tokens[0];
+                            access.put(lno, op);
+                        }
+                        else{
+                            if(type == 2){
+                                //op.setExpr(getPostfix(tokens));
+                            }
+                        }
+                    }
+                    else if(type != 2){
+                        if(type == 3){
+                            //its a jump
+                        }
+                        else{
+                            //var assignment
+                            //op.setType
+                            //op.setValue(new Num(tokes[i]);
+                            access.put(tokens[i], op);
+                        }
+                    }
+                }
+                program.add(op);
+                System.out.println(line);
+            }
+
+        }
     }
 }
--- LP-1/src/cs6301/g21/Operation.java
+++ LP-1/src/cs6301/g21/Operation.java
@@ -1,14 +1,38 @@
 package cs6301.g21;
 
-import java.util.HashMap;
 
 public class Operation {
+    private static int JUMP = 3;
+    private static int POST = 2;
+    private static int VAR = 1;
+    private static int PRINT = 0;
     private int type;
     private int jumps;
     private Num value;
-    private String var;
+    private String expression;
 
     public Operation(){
+        type = -1;
+        jumps = 0;
+        value = null;
+    }
+    public void setType(int type){
+        this.type = type;
+    }
+    public Num execute(){
+        switch(type){
+            case 3://JUMP
+                break;
+            case 2://POSTFIX
+                break;
+            case 1://VAR ASSIGNMENT
+                break;
+            case 0://PRINT VAR
+                System.out.print("");
+                //printList
+            default:
 
+        }
+        return new Num();
     }
 }
