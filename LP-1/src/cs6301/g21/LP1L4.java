b689e9f750b1bc3b4e1f546faff74aded8a89679
umangkshah <umang.k.shah@gmail.com>
2017-09-20 23:27:22 -0500

    Update printing logic

--- LP-1/src/cs6301/g21/LP1L4.java
+++ LP-1/src/cs6301/g21/LP1L4.java
@@ -2,98 +2,131 @@ package cs6301.g21;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayDeque;
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Scanner;
 
 public class LP1L4 {
+    private static int JUMP = 3;
+    private static int POST = 2;
+    private static int VAR = 1;
+    private static int PRINT = 0;
 
     public static void main(String args[])throws FileNotFoundException{
         ArrayList<Operation> program = new ArrayList<>();
         
 
-        if(args.length > 0){
-            Scanner in = new Scanner(new File(args[0]));
+        if(true){//argslength > 0 and file from cmd line
+            Scanner in = new Scanner(new File("/home/uks/ImplofAlgos/Implementation-of-Advanced-Algorithms/LP-1/src/cs6301/g21/input/lp1-l4-in2.txt"));
+            Operation.initChecker();
             while (in.hasNextLine()){
                 String line = in.nextLine();
                 if(line.equals(";"))
                     break;
                 Operation op = new Operation();
-                int type=-1;
-                if(line.contains("=")) {
-                    type = 1;
-                    op.setType(1);
-                    if (line.contains("+...")) {
-                        op.setType(2);
-                        type = 2;
-                    }
-                }else if(line.contains("?")){
-                    op.setType(3);
-                    type = 3;
-                }
                 String [] tokens = line.split(" ");
+                int type = -1;
+                op.setLevel(3);
                 for(int i=0;i < tokens.length;i++){
                     if(i==0) {
                         if (tokens[i].matches("\\d+")) {
                             String lno = tokens[0];
-                            Register.progReg.put(lno, program.size());
-
-                            if (type == 2) {
-                                //op.setExpr(getPostfix(tokens));
-                            }
-
+                            Register.putLineNo(lno, program.size());
+                            op.setLevel(4);
                         }else{
                             //var name
-                            if(type == 2){
-                                //expression
-                            }
-                            else{
-                                //var assmt
-
-                            }
+                            type = PRINT;
+                            op.setType(type);
+                            op.setVar(tokens[0]);
                         }
                     }
-                    else {
-                        if (type != 2) {
-                            if (type == 3) {
-                                //its a jump
-                                //[lineNo] var ? jump1 [: jump2]
-                                if (tokens[i].matches("\\d+")){
+                    else if(i == 1 && type < 0 && !tokens[i].matches("\\d+")){
 
-                                }
-                            } else {
-                                //var assignment
-                                //op.setType
-                                //op.setValue(new Num(tokes[i]);
-                                varReg.put(tokens[i], op);
+                        type = PRINT;
+                        op.setVar(tokens[1]);
+                        op.setType(type);
+                    }
+                    else {
+                        if(tokens[i].contains("=")){
+                            //either asmt or expression
+                            type = VAR;
+                            op.setType(type);
+                        }
+                        else if(tokens[i].matches("\\d+")){
+                            //either asmt or start of an expression
+                            if(type == VAR)
+                                op.setValue(tokens[i]);
+                        }
+                        else if(Operation.isValidOperator(tokens[i]) || (type == VAR && tokens[i].matches("\\[a-zA-Z]+"))){
+                            //expression
+                            Queue q = new ArrayDeque();
+                            String e = line.substring(line.indexOf("= ")+2);
+                            //q = convert(e);
+                            //op.setExpression(q);
+                            type = POST;
+                            op.setType(type);
+                            break;
+                        }
+                        else if(tokens[i].contains("?")){
+                            //JUMP
+                            String token = line.substring(line.indexOf("?")+1, line.indexOf(";"));
+                            token = token.replace(" ", "");
+                            if(token.contains(":")){
+                                String[] j = token.split(":");
+                                op.setJumps(j[0], j[1]);
+                            }else{
+                                op.setJumps(token, null);
                             }
+                            type = 3;
+                            op.setType(JUMP);
+
+                            break;
                         }
                     }
                 }
                 program.add(op);
-                System.out.println(line);
             }
-            for(int i=0;i < program.size(); i++){
-                Operation ptr = program.get(i);
+
+            for(int i=0;i < program.size();i++){
+                System.out.println(i + " > "+program.get(i));
+            }
+            int pc = 100;
+            while(pc < program.size()){
+                Operation ptr = program.get(pc);
                 int type = ptr.getType();
                 switch(type){
                     case 3://JUMP
-                        String lno = ptr.resolve(varReg.get(ptr.getVar()));
-                        i =
+                        String lno = ptr.resolve();
+                        if(lno == null)
+                            pc++;
+                        else
+                            pc = Register.getLineNo(lno);
                         break;
                     case 2://POSTFIX
                         Num ans;
-//                        ans = evaluate(ptr.getVar());
+//                        ans = evaluate(ptr.getExpression());
 //                        varRegister.put(ptr.getVar());
+                        pc++;
                         break;
                     case 1://VAR ASSIGNMENT
+                        Register.putVar(ptr.getVar(),ptr.getValue());
+                        pc++;
                         break;
                     case 0://PRINT VAR
-                        System.out.print("");
-                        //printList
+//                        Num outNum = Register.getVar(ptr.getVar());
+//                        System.out.print(outNum);
+//                        outNum.printList();
+                        pc++;
+                        break;
                     default:
-
+                        System.out.println("Unexpected Error");
+                        break;
+                }
+                if(ptr.getLevel() == 3){
+                    Num outNum = Register.getVar(ptr.getVar());
+                    System.out.println(outNum);
+                    outNum.printList();
                 }
             }
 
--- LP-1/src/cs6301/g21/Operation.java
+++ LP-1/src/cs6301/g21/Operation.java
@@ -15,6 +15,7 @@ public class Operation {
     private Num value;
     private Queue expression;
 	private String varName;
+	private int level;
 //	private
     public Operation(){
 
@@ -81,10 +82,36 @@ public class Operation {
 	}
 	
 	public String resolve(){
-		Num val_ = Register.varReg.get(this.varName);
+		Num val_ = Register.getVar(this.varName);
 		if(val_.compareTo(Register.zero_) == 1){
 			return jumps[0];
 		}
 		return jumps[1];
 	}
+
+	public void setLevel(int level){
+	    this.level = level;
+	}
+
+	public int getLevel(){
+	    return this.level;
+    }
+
+	public String toString(){
+	    StringBuilder s = new StringBuilder();
+	    s.append(" lev: "+level);
+	    s.append(" type: "+type);
+        s.append(" var: "+varName);
+	    if(type == JUMP) {
+            s.append(" ? " + jumps[0] + " : " + jumps[1] + " = ");
+            s.append(Register.getLineNo(jumps[0]) + " : " + Register.getLineNo(jumps[1]) + " ;");
+        }
+	    else if(type == POST)
+	        s.append("EXP: " + expression+" ;");
+        else if(type == VAR)
+            s.append((" Val = " + value+" ;"));
+        else
+            s.append(" print ;");
+	    return s.toString();
+    }
 }
