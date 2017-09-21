368682698f8fb398c77cb4d068f1479edf6faee0
umangkshah <umang.k.shah@gmail.com>
2017-09-20 22:27:51 -0500

    Helper func

--- LP-1/src/cs6301/g21/Register.java
+++ LP-1/src/cs6301/g21/Register.java
@@ -3,6 +3,23 @@ package cs6301.g21;
 import java.util.HashMap;
 
 public class Register{
-	static HashMap<String, Num> varReg;
-	static HashMap<String, Integer> progReg;
+	static HashMap<String, Num> varReg = new HashMap<>();
+	static HashMap<String, Integer> progReg = new HashMap<>();
+	static Num zero_ = new Num(0);
+
+	static void putVar(String var, Num val){
+		varReg.put(var, val);
+	}
+
+	static Num getVar(String var){
+		return varReg.get(var);
+	}
+
+	static void putLineNo(String lno, int pc){
+		progReg.put(lno, pc);
+	}
+
+	static int getLineNo(String lno){
+		return progReg.get(lno);
+	}
 }
