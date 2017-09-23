cfc3e604b795617e4470f642df5d159a67edd393
Shreya Vishwanath Rao <ShreyaRao09@users.noreply.github.com>
2017-09-23 17:45:01 -0500

    Update PostfixEvaluator.java

--- LP-1/src/cs6301/g21/PostfixEvaluator.java
+++ LP-1/src/cs6301/g21/PostfixEvaluator.java
@@ -1,5 +1,7 @@
 package cs6301.g21;
 
+import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Stack;
 
@@ -20,19 +22,19 @@ public class PostfixEvaluator {
 	public static Num evaluate(Queue<String> q){
 
 		Stack<Num> stack = new Stack<Num>();
-		Tokens T= new Tokens();
+		ShuntingYard t= new ShuntingYard();
 		
 		for(String token : q ){
 			
-			if(Tokens.isNumeric(token)){
+			if(ShuntingYard.isNumeric(token)){
 				Num n= new Num(token);
 				stack.push(n);
 			}
-			else if(Tokens.isVariable(token)){
+			else if(ShuntingYard.isVariable(token)){
 				stack.push(Register.varReg.get(token));
 				
 			}
-			else if(Tokens.isValidOperator(token)){ //Shunting Yard function
+			else if(ShuntingYard.isValidOperator(token)){ //Shunting Yard function
 				Num right;
                			Num left;
 
@@ -46,14 +48,10 @@ public class PostfixEvaluator {
 				    left = stack.pop();
 				    stack.push(Num.subtract(left, right));
 
-                }else if(token.equals("*")){
-                    right = stack.pop();
-                    left = stack.pop();
-                    if(left.compareTo(right) == 0)
-                        stack.push(Num.power(left, 2));
-                    else
-                        stack.push(Num.product(left, right));
-
+				}else if(token.equals("*")){
+				    right = stack.pop();
+				    left = stack.pop();
+				    stack.push(Num.product(left, right));
 
 				}else if(token.equals("/")){
 				    right = stack.pop();
@@ -80,38 +78,38 @@ public class PostfixEvaluator {
 			}
 		}
 		return stack.pop();
+		
 	}
 	
-//	public static void main(String[] args){
-//		Queue<String> queue= new LinkedList<String>();
-//
-//		//infix = 5 + 6 - 7  * (3+7);
-//		//postfix = 56+737+*-
-//
-//		Register.varReg = new HashMap<String,Num>();
-//		Register.varReg.put("x", new Num(5));
-//		Register.varReg.put("y", new Num(6));
-//		Register.varReg.put("z", new Num(7));
-//		Register.varReg.put("a", new Num(3));
-//
-//		queue.add("x");
-//		queue.add("y");
-//		queue.add("+");
-//		queue.add("z");
-//		queue.add("a");
-//		queue.add("z");
-//		queue.add("+");
-//		queue.add("*");
-//		queue.add("-");
-//
-//
-//		Num result = evaluate(queue);
-//		//printing the sign since to string doesn't display negative sign
-//		System.out.println(result.getSign() + " " + result.toString());
-//		result.printList();
-//
-//	}
+// 	public static void main(String[] args){
+// 		Queue<String> queue= new LinkedList<String>();
+		
+// 		//infix = 5 + 6 - 7  * (3+7);
+// 		//postfix = 56+737+*-
+		
+// 		Register.varReg = new HashMap<String,Num>();
+// 		Register.varReg.put("x", new Num(5));
+// 		Register.varReg.put("y", new Num(6));
+// 		Register.varReg.put("z", new Num(7));
+// 		Register.varReg.put("a", new Num(3));
 
-
+// 		queue.add("x");
+// 		queue.add("y");
+// 		queue.add("+");
+// 		queue.add("z");
+// 		queue.add("a");
+// 		queue.add("z");
+// 		queue.add("+");
+// 		queue.add("*");
+// 		queue.add("-");
+		
+		
+// 		Num result = evaluate(queue);
+// 		//printing the sign since to string doesn't display negative sign
+// 		System.out.println(result.getSign() + " " + result.toString());
+// 		result.printList();
+		
+// 	}
+	
 
 }
