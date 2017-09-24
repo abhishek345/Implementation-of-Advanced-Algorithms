e78ab95e5f43fea510aedb5684cc1876dce07b46
Vibha Belavadi <VibhaBelavadi@users.noreply.github.com>
2017-09-24 13:49:03 -0500

    Updated Q4 with java doc

--- SP4/Q4.java
+++ SP4/Q4.java
@@ -2,19 +2,33 @@ package cs6301.g21;
 
 public class Q4 {
 
-    // Preconditions: arr[0..n-1] is sorted, and arr[0] <= x < arr[n-1].
-    // Returns index i such that arr[i] <= x < arr[i+1].
+    /**
+     * Preconditions: arr[0..n-1] is sorted, and arr[0] <= x < arr[n-1].
+     * Returns index i such that arr[i] <= x < arr[i+1].
+     * @param arr : Sorted array
+     * @param x : element to be investigated
+     * @param <T> : Type of the array/element
+     * @return int : index of the array arr that has the result
+     */
     public static<T extends Comparable<? super T>> int binarySearch(T[] arr, T x){
 
         int low = 0;
         int high = arr.length-1;
-        int avg;
-
+        
         int answer = binarySearch(arr, low, high, x);
 
         return answer;
     }
 
+    /**
+     * Recursive version of binary search
+     * @param arr : Sorted array with
+     * @param low : lower index for binary search
+     * @param high : high index for binary search
+     * @param x : Element to be compared
+     * @param <T> : Type of array/element
+     * @return int: index of the array arr that has the result
+     */
     public static<T extends Comparable<? super T>> int binarySearch(T[] arr, int low, int high, T x){
 
         int avg = (low+high)/2;
