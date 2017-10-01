5ec75765bf0aefe23456b03f726152160d8e57e2
umangkshah <umang.k.shah@gmail.com>
2017-10-01 11:26:13 -0500

    done Q2

--- SP5/DualPivotPartition.java
+++ SP5/DualPivotPartition.java
@@ -1,74 +1,60 @@
 package cs6301.g21;
 
-import java.util.Random;
+/**Implementation of Quicksort with Dual Pivot Parition
+ * @author Umang Shah, Vibha Belavadi, Abhishek Jagwani, Shreya Vishwanath Rao
+ * @version 1.0 : 09/30/2017
+ *
+ */
 
 public class DualPivotPartition{
-  static Random rd;
+  /*
+  public static void main(String[] args) {
+    // int arr[] = {6, 5, 7, 3, 1, 8, 2, 9};
+    // dualPivotQS(arr, 0, arr.length-1);
+    // System.out.println("printing array: ");
+    // for(int a: arr)
+    //   System.out.println(a);
+    // NEEDS A DRIVER
+  }*/
 
+  /** swaps value at 2 indices in an array
+   *
+   * @param A Integer Array
+   * @param x index 1
+   * @param y index 2
+   */
   public static void swap(int[] A, int x, int y){
     int tmp = A[x];
     A[x] = A[y];
     A[y] = tmp;
   }
 
-  public static void circleSwap(int[] A,  int x, int y, int z){
-    int tmp = A[y];
-    A[y] = A[x];
-    A[x] = A[z];
-    A[z] = tmp;
-  }
-
-  public static void quickSort(int[] A, int p,int r){
-    System.out.println("qs: " + p + " , "+r);
-
-
+  /** sort an array using quicksort with dual pivot partition
+   *  using insertion sort for size below 18
+   *  reference : DualPivotQuicksort by Yaroslavskiy
+   *  http://codeblab.com/wp-content/uploads/2009/09/DualPivotQuicksort.pdf
+   *
+   * @param A Integer Array
+   * @param p start index
+   * @param r end index
+   */
+  public static void dualPivotQS(int[] A, int p, int r){
     if(p >= r) return;
-    if(r-p < 2){
-      InsertionSort.nSquareSort(A,p,r);
+    if(r-p < 18){
+      InsertionSort.nSquareSort(A, p, r);
       return;
     }
-    rd = new Random();
-    int ans[] = partition(A, p, r);
-    int x1 = ans[0];
-    int x2 = ans[1];
-    int i = ans[2];
-    int j = ans[3];
-    int k = ans[4];
-    // System.out.println("x1 x2 i j k : ");
-    // for(int x: ans)
-    //   System.out.print(" > " +x);
-    // System.out.println();
-    quickSort(A, p, k-1);
-    quickSort(A, j+1, r);
-    // if(x1 != x2){
-    quickSort(A, k+1, j-1);
-    // }
-    // System.out.println("printing array: ");
-    // for(int a: A)
-    //   System.out.println(a);
-  }
-
-  public static int[] partition(int[] A, int p, int r){
-    int p1,p2;
-    p1 = rd.nextInt(r-1) + p;
-    p2 = rd.nextInt(r-1) + p;
-    if(A[p1] > A[p2]){
-      int tmp = p1;
-      p1 = p2;
-      p2 = tmp;
-    }
-    System.out.println(" pivots = " + p1 + " , " + p2);
-    swap(A, p1, p);
-    swap(A, p2, r);
-    int x1 = A[p];
-    int x2 = A[r];
+    if(A[p] > A[r])
+      swap(A,p,r);
+    int x1 = A[p]; int x2 = A[r];
     int i = p+1; int k = p+1; int j = r-1;
+
     while(i <= j){
       if(A[i] < x1){
         swap(A, i , k); k++;
       }
-      else if(A[i] >= x2){
-        while(A[j] > x2 && i < j) j--;
+      else if(A[i] > x2){
+        while(i < j && A[j] > x2) j--;
         swap(A, i, j);
         j--;
         if(A[i] < x1){
@@ -79,19 +65,12 @@ public class DualPivotPartition{
     }
     k--;
     j++;
+    swap(A, p, k); swap(A, r, j);
 
-    swap(A, p, k);
-    swap(A, r, j);
-    int ans[] = {x1, x2, i, j, k};
-    return ans;
+    dualPivotQS(A, p, k-1);
+    if(x1 != x2) dualPivotQS(A, k+1, j-1);
+    dualPivotQS(A, j+1, r);
   }
 
-  public static void main(String[] args) {
-    int arr[] = {6, 5, 7, 3, 1, 8, 2, 9};
-    quickSort(arr, 0, arr.length-1);
-    //swap(arr, 0, 3);
-    System.out.println("printing array: ");
-    for(int a: arr)
-      System.out.println(a);
-  }
+
 }
