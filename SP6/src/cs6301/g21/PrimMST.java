2f5ddea3fac6b00e3efe9fb94957bc2c3f18f5bf
Abhishek Jagwani <abhi.jagwani@gmail.com>
2017-10-08 16:17:06 -0500

    Prim2

--- SP6/src/cs6301/g21/PrimMST.java
+++ SP6/src/cs6301/g21/PrimMST.java
@@ -1,17 +1,29 @@
-
-/* Ver 1.0: Starter code for Prim's MST algorithm */
-
 package cs6301.g21;
 
+import java.util.Comparator;
+import java.util.Iterator;
 import java.util.Scanner;
-import java.lang.Comparable;
+import cs6301.g21.GraphExtended3.GEVertex;
 import java.io.FileNotFoundException;
 import java.io.File;
 
+/**
+ * Implementation of Prim's algorithm using Indexed heaps
+ * @author Shreya Vishwanath Rao, Abhishek Jagwani, Umang Shah, Vibha Belavadi
+ * @version 1.0:08/29/2017
+ * @since 1.0
+ */
+
 public class PrimMST {
     static final int Infinity = Integer.MAX_VALUE;
-
+    static GraphExtended3 ge;
+   
+    /**
+     * Constructor of the class PrimMST
+     * @param g : input graph
+     */
     public PrimMST(Graph g) {
+    	ge = new GraphExtended3(g);
     }
 
     public int prim1(Graph.Vertex s) {
@@ -22,14 +34,57 @@ public class PrimMST {
         return wmst;
     }
 
+    
+    /**
+     * Implementation of the Prim's algorithm version 2 using Indexed heaps
+     * @param s : Starting Vertex of the graph to implement Prim's algorithm
+     * @return : weight of the minimum spanning tree
+     */
     public int prim2(Graph.Vertex s) {
         int wmst = 0;
 
-        // SP6.Q6: Prim's algorithm using IndexedHeap<PrimVertex>:
-
+        GraphExtended3.GEVertex[] arrayOfVertices = new GraphExtended3.GEVertex[ge.size() +1];
+        Comparator<GraphExtended3.GEVertex> comparator = (Comparator<GEVertex>) new ComparatorVertex.VertexComparator();
+		int i=0;
+        Iterator it = ge.iterator();
+		while(it.hasNext())
+		{
+			Graph.Vertex u = (Graph.Vertex)it.next();
+			ge.setSeen(u,false);
+			ge.setParent(u,null);
+			ge.setDist(u,Infinity);
+			ge.setIndex(u,i);
+			arrayOfVertices[i++] = ge.getGEVertex(u);	
+		}
+		
+		ge.setDist(s,0);
+		
+		IndexedHeap<GraphExtended3.GEVertex> indexedHeaps = new IndexedHeap<GraphExtended3.GEVertex>(arrayOfVertices, comparator, ge.size()-1);
+        
+		while(indexedHeaps.size > 0){
+			GraphExtended3.GEVertex otherVertex = indexedHeaps.remove();
+			otherVertex.setSeen(true);
+			wmst += otherVertex.dist;
+			for(Graph.Edge e : otherVertex.adj){
+				Graph.Vertex tempV = e.otherEnd(otherVertex.getElement());
+				GraphExtended3.GEVertex v= ge.getGEVertex(tempV);
+				if(!v.seen && e.weight < v.dist){
+					v.dist = e.weight;
+					v.parent = otherVertex.getElement();
+					indexedHeaps.percolateUp(v.getIndex());
+					
+				}
+			}
+		}
+		
         return wmst;
     }
 
+    /**
+     * Main Function
+     * @param args : command line Input
+     * @throws FileNotFoundException : In case of file not found
+     */
     public static void main(String[] args) throws FileNotFoundException {
 	Scanner in;
 
