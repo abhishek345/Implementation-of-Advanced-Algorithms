082122ca72aefa6c68460e8b2a8f906e8c64bc08
Abhishek Jagwani <abhi.jagwani@gmail.com>
2017-10-22 19:22:05 -0500

    javadoc

--- SP7/LongestStreak.java
+++ SP7/LongestStreak.java
@@ -4,11 +4,22 @@ import java.util.Iterator;
 import java.util.Scanner;
 import java.util.TreeSet;
 
+/**
+ * Implementation of length of the longest streak of consecutive integers
+ * @author  Abhishek Jagwani, Shreya Vishwanath Rao, Umang Shah, Vibha Belavadi
+ * @version 1.0:10/15/2017
+ * @since 1.0
+ */
+
 public class LongestStreak {
 
 	static int LScountCurr = 1;
 	static int LScountFinal = 1;
-	
+	/**
+	 * find out length of the longest streak of consecutive integers
+	 * @param A : input array 
+	 * @return : length of the longest streak
+	 */
 	static int longestStreak(int[] A){
 			TreeSet<Integer> tSet = new TreeSet<>();
 			int prev = 0, curr = 0;
@@ -35,6 +46,10 @@ public class LongestStreak {
 			return LScountFinal;
 	}
 	
+	/**
+	 * Main function
+	 * @param args : command line input
+	 */
 	public static void main(String[] args){
 		Scanner in = new Scanner(System.in);
 		System.out.println("Enter the size of array");
