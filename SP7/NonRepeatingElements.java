6484cf9daa89e651539f517dace59cb70af9eac0
Abhishek Jagwani <abhi.jagwani@gmail.com>
2017-10-22 19:21:34 -0500

    javadoc

--- SP7/NonRepeatingElements.java
+++ SP7/NonRepeatingElements.java
@@ -5,8 +5,21 @@ import java.util.Map;
 import java.util.Scanner;
 import java.util.TreeMap;
 
+/**
+ * Given an array A, return an array B that has those elements of A that
+ * occur exactly once, in the same order in which they appear in A
+ * @author  Abhishek Jagwani, Shreya Vishwanath Rao, Umang Shah, Vibha Belavadi
+ * @version 1.0:10/15/2017
+ * @since 1.0
+ */
+
 public class NonRepeatingElements {
 
+	/**
+	 * finds out elements that occur only once in input array
+	 * @param A : input Array
+	 * @return : Array returning elements which are present exactly once
+	 */
 	public<T extends Comparable<? super T>> Comparable[] exactlyOnce(T[] A){
 		
 		ArrayList<T> bList = new ArrayList<T>();
@@ -27,14 +40,18 @@ public class NonRepeatingElements {
 		Comparable[] bArray= new Comparable[bList.size()];
 		int count = 0;
 	
-		for(T element : bList){
-			bArray[count] = element;
+		for(T element: bList){
+			bArray[count]=element;
 			count++;
 		}
 		
 		return bArray;	
 	}
 	
+	/**
+	 * Main function
+	 * @param args : command line input
+	 */
 	public static void main(String args[]){
 		
 		NonRepeatingElements nre = new NonRepeatingElements();
