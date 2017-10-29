e9a55e025aafa625d65296b54a57a6fff61ec488
Vibha Belavadi <vibha.belawadi@gmail.com>
2017-10-29 10:48:38 -0500

    bfsTake2

---
+++ LP3/src/cs6301/g21/bfsTake2.java
@@ -0,0 +1,74 @@
+/** Breadth-first search
+ *  originally by @author rbk
+ *  Modified to run on XGraph and return edges
+ *
+ */
+
+package cs6301.g21;
+
+
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+
+public class bfsTake2 {
+    public static final int INFINITY = Integer.MAX_VALUE;
+    private static XGraph.XVertex src;
+    private static XGraph ge;
+
+
+    // reinitialize allows running bfsTake2 many times, with different sources
+    static void reinitialize(XGraph xGraph, XGraph.XVertex newSource) {
+        src = newSource;
+        ge = xGraph;
+
+        XGraph.XGraphIterator it = (XGraph.XGraphIterator) ge.iterator();
+        while (it.hasNext()){
+            XGraph.XVertex xcur = (XGraph.XVertex) it.next();
+            xcur.seen = false;
+            xcur.parent = null;
+            xcur.dis = INFINITY;
+        }
+
+        ge.getVertex(src).dis = 0;
+    }
+
+    public static List<XGraph.XEdge> BFS(XGraph ge, XGraph.XVertex newSource) {
+
+        reinitialize(ge, newSource);
+
+        Queue<XGraph.XVertex> q = new LinkedList<>();
+        List<XGraph.XEdge> bfsEdges = new LinkedList<>();
+        q.add(src);
+
+        while(!q.isEmpty()) {
+            XGraph.XVertex u = q.remove();
+            Iterator<Graph.Edge> edgeIterator = u.iterator();
+
+            XGraph.XEdge x;
+            if(!edgeIterator.hasNext())
+                continue;
+            else {
+                x = (XGraph.XEdge) edgeIterator.next();
+                if(x.getWeight() == 0) {
+                    XGraph.XVertex v = (XGraph.XVertex) x.otherEnd(u);
+                    if (!v.getSeen() && (v.getVCno() == u.getVCno())) {
+                        visit(u, v);
+                        q.add(v);
+                        bfsEdges.add(x);
+                    }
+                }
+            }
+        }
+
+        return bfsEdges;
+    }
+
+    static void visit(XGraph.XVertex u, XGraph.XVertex v) {
+        v.setSeen(true);
+        v.setParent(u);
+        v.dis = u.dis+1;
+    }
+}
