9925121511ff22985334cb50e003287c4e9441b2
Abhishek Jagwani <abhi.jagwani@gmail.com>
2017-11-11 19:41:35 -0600

    Part C done

--- LP-4/src/cs6301/g21/BellmanFord.java
+++ LP-4/src/cs6301/g21/BellmanFord.java
@@ -11,92 +11,99 @@ import cs6301.g21.XGraph.XVertex;
 import java.io.File;
 import java.io.FileNotFoundException;
 
+
+/**
+ * Determining the Shortes Paths of all vertices in a graph.
+ *
+ *
+ * @author Umang Shah, Shreya Vishwanath Rao, Abhishek Jagwani, Vibha Belavadi
+ * @version 1.0: 2017/09/13
+ *
+ */
+
 public class BellmanFord {
 	
 	
 	private static final int INFINITY = Integer.MAX_VALUE;
 
-	public static boolean BellmanFord(XGraph g, Vertex sourceNode){
-		System.out.println("Inside BellmanFord, Source is "+sourceNode.getName());
-		boolean noChange = false;
-		XGraphExtended ge= new XGraphExtended(g);
-		Iterator<Vertex> it = g.iterator();
-		
-		System.out.println("For k = 0" );
-		
-		while(it.hasNext()){
-			XGraph.XVertex s= (XGraph.XVertex) it.next();
-			ge.setDistance(s, 0, INFINITY);
-			s.parent = null;
-			System.out.println(1+s.getName()+ " "+ ge.getDistance(s, 0));
-		}
-		XGraph.XVertex source = g.getVertex(sourceNode);
-		ge.setDistance(source, 0, 0);
-		System.out.println(1+source.getName()+ " "+ ge.getDistance(source, 0));
-		System.out.println("Initialized");
-		
-		for(int k=1;k<=g.size();k++){
-			System.out.println("For k ="+k );
-			noChange = true;
-			it = g.iterator();
-			
-	 		while(it.hasNext()){
-				XGraph.XVertex u= (XGraph.XVertex) it.next();
-				int val=ge.getDistance(u, k-1);
-				ge.setDistance(u, k, val); //u.d[k]=u.d[k-1];
-				System.out.println(1+u.getName()+ " "+ ge.getDistance(u, k));
-				
-				Iterator<Edge> itEdge= u.revIterator();
-				while(itEdge.hasNext()){
-					XEdge e= (XEdge)itEdge.next();
-					XVertex p= (XVertex) e.otherEnd(u); //Little confusion. check again
-					
-					if(ge.getDistance(u,k)>ge.getDistance(p, k-1) + e.getWeight()){
-						int newDist=ge.getDistance(p, k-1) + e.getWeight();
-						ge.setDistance(u, k, newDist);
-						u.setParent(p);
-						noChange=false;
-					}
-					System.out.println(1+u.getName()+ " "+ ge.getDistance(u, k));
-				}
-				System.out.println();
-			}
-	 		if(noChange){
-	 			it = g.iterator();
-	 			while(it.hasNext()){
-					XGraph.XVertex u= (XGraph.XVertex) it.next();
-					u.setDis(ge.getDistance(u, k));
-	 			}
-	 			return true;
-	 		}
-		}
-		return false;
-	}
-	
-	public static int shortestPath(XGraph g, Vertex sourceNode, Vertex target){
-		XVertex s= g.getVertex(sourceNode);
-		XVertex t= g.getVertex(target);
-		System.out.println("in shortest path, source: "+s.getName()+ ", target: "+ t.getName());
-		if(BellmanFord(g,s)){
-			return 5;
-		}
-		else{
-			return INFINITY;
-		}
-	}
-	
-	public static void main(String[] args) throws FileNotFoundException{
-		
-		java.util.Scanner in = new java.util.Scanner(new File(args[0]));
-		Graph g = Graph.readDirectedGraph(in);
+	/**Counts the number of shortest paths in the graph
+	 * 
+	 * @param g : Input Graph G
+	 * @param s : Source Vertex
+	 * @param end : Target Vertex
+	 * @return : number of Shortest Paths
+	 */
+	public static int getPaths(XGraph g, Vertex s, Graph.Vertex end){
 
-		int source = in.nextInt();
-		int target = in.nextInt();
-		
-		XGraph ge = new XGraph(g);
-		Vertex s= g.getVertex(source);
-		Vertex t= g.getVertex(target);
-    	System.out.println(BellmanFord.shortestPath(ge,s,t));
-	    }
+        //get a distance array
+        boolean noChange = false;
+        int numVert = g.n;
+        int [][] vertDist = new int[numVert][g.m];
+        int[] parentCount= new int[numVert];
+
+        //initialize all the distances
+        for(int i=0; i<numVert; i++){
+            vertDist[i][0] = Integer.MAX_VALUE;
+            parentCount[i]=0;
+            for(int j=1; j<g.m; j++)
+                vertDist[i][j] = 0;
+        }
+
+        //set parents to be null
+        for(Graph.Vertex u : g){
+            ((XGraph.XVertex) u).setParent(null);
+        }
+
+        //initialise distance for source to be zero:
+        vertDist[s.name][0] = 0;
+        parentCount[s.name]=1;
+
+        //go through as many iterations as number of vertices required
+        for(int i=1; i<=g.n; i++){
+            noChange = true;
+
+            
+            for(int k=0; k<numVert; k++){
+            	parentCount[k]=0;
+            }
+            parentCount[s.name]=1;
+            
+            
+            for(Graph.Vertex u : g){
+                vertDist[u.name][i] = vertDist[u.name][i-1];
+                Iterator<Graph.Edge> edgeIterator = ((XGraph.XVertex) u).revIterator();
+
+                while (edgeIterator.hasNext()){
+                    Graph.Edge e = edgeIterator.next();
+                    Graph.Vertex p = e.from;
+
+                    if(vertDist[p.name][i-1] != Integer.MAX_VALUE && vertDist[u.name][i] >= (vertDist[p.name][i-1] + e.getWeight())){
+                    	if(vertDist[u.name][i] > (vertDist[p.name][i-1] + e.getWeight())){
+	                        vertDist[u.name][i] = (vertDist[p.name][i-1] + e.getWeight());
+	                        ((XGraph.XVertex) u).parent = (XGraph.XVertex) p;
+	                        noChange = false;
+                    	}
+                    		if(vertDist[u.name][i] > (vertDist[p.name][i-1] + e.getWeight()))
+                    			parentCount[u.name]=parentCount[p.name];
+                    		else
+                    			parentCount[u.name]+=parentCount[p.name];
+                    	
+                    	
+                    }
+                }
+            }
+
+            //if no change update the distance
+            if(noChange){
+                for(Graph.Vertex u : g){
+                    ((XGraph.XVertex) u).dis = vertDist[u.name][i];
+                }
+                return parentCount[end.name];
+            }
+        }
+
+        return vertDist[end.name][g.m-1];
+    }
+
 
 }
