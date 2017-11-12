3a3cc5743c4d53ad25c4227b0a114c385d20a748
Vibha Belavadi <VibhaBelavadi@users.noreply.github.com>
2017-11-11 21:51:08 -0600

    Required dependencies for a and b part

---
+++ LP-4/src/cs6301/g21/Index.java
@@ -0,0 +1,7 @@
+// Ver 1.0:  Index interface
+
+package cs6301.g21;
+public interface Index {
+    public void putIndex(int index);
+    public int getIndex();
+}
---
+++ LP-4/src/cs6301/g21/IndexedHeap.java
@@ -0,0 +1,41 @@
+package cs6301.g21;
+
+import java.util.Comparator;
+
+public class IndexedHeap<T extends Index> extends BinaryHeap<T> {
+
+
+    IndexedHeap(T[] q, Comparator<T> comp, int n) {
+        super(q, comp, n);
+    }
+
+    void decreaseKey(T x) {
+        percolateUp(x.getIndex());
+    }
+
+    void buildHeap() {
+        super.buildHeap();
+        updateIndices();
+
+    }
+
+    void percolateUp(int i) {
+        super.percolateUp(i);
+        updateIndices();
+    }
+
+    void percolateDown(int i) {
+        super.percolateDown(i);
+        updateIndices();
+    }
+
+    //update indices after any operation
+    void updateIndices(){
+
+        for (int j = 0; j < size; j++) {
+            T temp = pq[j];
+            temp.putIndex(j);
+        }
+    }
+
+}
